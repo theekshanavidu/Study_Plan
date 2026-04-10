fb47e9ca1bec110b55543430709e6f2d0b6e36a6
theekshanavidu <151367679+theekshanavidu@users.noreply.github.com>
2026-04-10 15:09:09 +0530

    Update service worker registration log message to v9

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('./sw.js')
             .then(reg => {
-                console.log('Service Worker v8 Registered');
+                console.log('Service Worker v9 Registered');
             })
             .catch(err => {
                 console.log('Registration failed:', err);
